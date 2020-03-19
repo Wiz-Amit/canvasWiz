14768ee1008de44424053f2a66306500ba1ae81c
amit-viacon <amit.viacon@gmail.com>
2020-03-19 13:02:08 +0530

    testing with comment

--- script.ts
+++ script.ts
@@ -95,6 +95,7 @@ class CanvasUtils extends CanvasRenderingContext2D {
       mouse.y = e.clientY;
 
       // console.log(mouse);
+      // testing git branch
     });
 
     window.addEventListener("mouseover", () => {
